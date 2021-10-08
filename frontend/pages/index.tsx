a7aa5df756310c60a4270d963c3f99c2eaa98c8c
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-08 16:53:50 +0900

    update

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,36 @@
+import axios from 'axios'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState(true)
+  const [questions, setQuestions] = useState<{id: string; title: string}[]>([])
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      const { worker } = require('../mocks/browsers')
+      worker.start()
+    }
+    async function fetchData() {
+      const response = await axios.get<{id: string; title: string}[]>('/novels');
+      return response
+    }
+
+    fetchData()
+      .then(res => {
+        setQuestions(res.data);
+      })
+      .catch(e => {
+        alert(e);
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [loading]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,58 +40,19 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {questions.map(item => (
+          <li key={item.id}>
+            <Link
+              href={{
+                pathname: '/questions/[slug]',
+                query: { slug: item.id },
+              }}
+            >
+              <a>{item.title}</a>
+            </Link>
+          </li>
+        ))}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
---
+++ frontend/pages/questions/[id].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const QuestionIndex: NextPage = () => {
+  return (
+    <div>
+      <h1>質問</h1>
+    </div>
+  )
+}
+
+export default QuestionIndex;
