28bb280b6ae27d4b7243170a5ce1b636b4f4e2ed
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-10 06:04:35 +0900

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +9,16 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, config }) {
+      addBase({
+        'h1': { fontSize: config('theme.fontSize.3xl') },
+        'h2': { fontSize: config('theme.fontSize.2xl') },
+        'h3': { fontSize: config('theme.fontSize.xl') },
+        'h4': { fontSize: config('theme.fontSize.lg') },
+        'h5': { fontSize: config('theme.fontSize.base') },
+        'h6': { fontSize: config('theme.fontSize.sm') },
+      })
+    })
+  ],
 }
