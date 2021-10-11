08c1b9f81d215a6461cbaa69bcf56f57f5639c28
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-11 20:43:46 +0900

    create logout module

---
+++ frontend/src/module/authentication/logout/reducer/logoutReducer.ts
@@ -0,0 +1,5 @@
+const START_LOGOUT = "START_LOGOUT" as const;
+const SUCCESS_LOGOUT = "SUCCESS_LOGOUT" as const;
+const FAIL_LOGOUT = "FAIL_LOGOUT" as const;
+
+export const logoutReducer = () => {}
