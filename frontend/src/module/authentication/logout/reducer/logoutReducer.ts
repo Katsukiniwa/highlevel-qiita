19efbe3d6e2e6165d3f362cba6e66c12e02cc0e6
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-11 21:55:38 +0900

    create logoutReducer

--- frontend/src/module/authentication/logout/reducer/logoutReducer.ts
+++ frontend/src/module/authentication/logout/reducer/logoutReducer.ts
@@ -1,5 +1,47 @@
+import { LoginState } from "../../login";
+
 const START_LOGOUT = "START_LOGOUT" as const;
 const SUCCESS_LOGOUT = "SUCCESS_LOGOUT" as const;
 const FAIL_LOGOUT = "FAIL_LOGOUT" as const;
 
-export const logoutReducer = () => {}
+const startLogoutAction = () => {
+  return { type: START_LOGOUT };
+};
+
+const successLogoutAction = () => {
+  return { type: SUCCESS_LOGOUT, payload: true };
+};
+
+const failLogoutAction = () => {
+  return { type: FAIL_LOGOUT };
+};
+
+export type LogoutActionType =
+  | ReturnType<typeof startLogoutAction>
+  | ReturnType<typeof successLogoutAction>
+  | ReturnType<typeof failLogoutAction>;
+
+  export const logoutReducer = (state: LoginState, action: LogoutActionType): LoginState => {
+    switch (action.type) {
+      case START_LOGOUT:
+        return {
+          ...state,
+          isLoading: true,
+          login: false,
+        };
+      case SUCCESS_LOGOUT:
+        return {
+          ...state,
+          isLoading: false,
+          login: action.payload,
+        };
+      case FAIL_LOGOUT:
+        return {
+          ...state,
+          isLoading: false,
+          login: false,
+        };
+      default:
+        return state;
+    }
+  };
