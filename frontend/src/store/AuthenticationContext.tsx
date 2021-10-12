54fa27aafa9c8ea8ad2e90726239c021a8c63e4f
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-12 20:19:37 +0900

    update AuthenticationContext

--- frontend/src/store/AuthenticationContext.tsx
+++ frontend/src/store/AuthenticationContext.tsx
@@ -1,26 +1,26 @@
 import { createContext, Dispatch, ReactChild, useReducer } from "react"
-import { loginReducer, LoginActionType } from "../module/authentication/login"
-import { logoutReducer, LogoutActionType } from "../module/authentication/logout"
+import { loginReducer, LoginActionType, LoginState } from "../module/authentication/login"
+// import { logoutReducer, LogoutActionType } from "../module/authentication/logout"
 
-export const AuthenticationContext = createContext({ isLoading: false, login: false })
+export const AuthenticationContext = createContext<LoginState>({ isLoading: false, login: false, error: null })
 
 //@ts-ignore
 export const LoginContext = createContext<Dispatch<LoginActionType>>(null)
 //@ts-ignore
-export const LogoutContext = createContext<Dispatch<LogoutActionType>>(null)
+// export const LogoutContext = createContext<Dispatch<LogoutActionType>>(null)
 
 const initialAuthenticationState = { isLoading: false, login: false, error: null }
 
 export function AuthenticationContextProvider({ children }: { children: ReactChild }) {
   const [authentication, login] = useReducer(loginReducer, initialAuthenticationState)
-  const [_authentication2, logout] = useReducer(logoutReducer, initialAuthenticationState)
+  // const [_authentication2, logout] = useReducer(logoutReducer, initialAuthenticationState)
 
   return (
     <AuthenticationContext.Provider value={authentication}>
       <LoginContext.Provider value={login}>
-        <LogoutContext.Provider value={logout}>
-          {children}
-        </LogoutContext.Provider>
+        {children}
+        {/* <LogoutContext.Provider value={logout}>
+        </LogoutContext.Provider> */}
       </LoginContext.Provider>
     </AuthenticationContext.Provider>
   )
