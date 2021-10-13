4641df0ea958a86ae8c719db5116d0258ac21217
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-13 20:54:25 +0900

    delete needless code

--- backend/spec/models/question_spec.rb
+++ backend/spec/models/question_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it "empty title throw error" do
     question = Question.new(title: nil, content: 'sample text')
-    question.valid?
     expect{ question.save! }.to raise_error(ActiveRecord::RecordInvalid)
   end
 end
