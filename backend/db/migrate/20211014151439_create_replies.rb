3a0362e8c451342bd7d1e30bb9868bec13d6fcd1
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-15 00:16:24 +0900

    create reply model

---
+++ backend/db/migrate/20211014151439_create_replies.rb
@@ -0,0 +1,9 @@
+class CreateReplies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :replies do |t|
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
