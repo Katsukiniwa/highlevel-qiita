fe5754e8fd88bcd6fc8f3100cd1c7c798e53eace
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-14 09:24:06 +0900

    add many to many relationship

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -1,8 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  validates :user_id, presence: true
+  has_many :tag, through: :tagging
 
+  validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 200 }
   validates :content, presence: true
 end
--- backend/app/models/tag.rb
+++ backend/app/models/tag.rb
@@ -1,2 +1,3 @@
 class Tag < ApplicationRecord
+  has_many :questions, through: :tagging
 end
