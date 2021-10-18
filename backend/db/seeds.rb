defbb8b0c33a9ccca1ca6f95139d21b34c6d6928
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-18 22:27:05 +0900

    create seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,8 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Question.create(title: 'sample')
+hashed_password = User.digest('password')
+user = User.new(
+  name: 'Katsukiniwa',
+  password: 'password',
+  password_confirmation: 'password',
+  email: 'katsukiniwa@test.com',
+  icon: 'https://example.com/katsukiniwa-icon.png'
+)
+user.save!
+User.create(
+  name: 'Eric Evans',
+  password: 'password',
+  password_digest: hashed_password,
+  email: 'ericevans@test.com',
+  icon: 'https://example.com/katsukiniwa-icon.png'
+)
+Category.create(
+  name: 'DDD',
+  name_en: 'ddd',
+  icon: 'https://example.com/ddd-icon.png'
+)
+user = User.find(1)
+eric = User.find(2)
+category = Category.find(1)
+user.questions.create(
+  title: 'What is Aggregate?',
+  content: "I don't understand what aggregate is.",
+  category_id: category.id
+)
+question = Question.find(1)
+question.answers.create(
+  user_id: 2,
+  content: 'Aggregate is root entity for domain object.'
+)
