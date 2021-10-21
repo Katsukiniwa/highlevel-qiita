6d8deffd77296fa498380be4e1c17317f6391ee0
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-21 23:01:59 +0900

    update

--- frontend/src/components/object/AnswerCard.tsx
+++ frontend/src/components/object/AnswerCard.tsx
@@ -1,6 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 export const AnswerCard = () => {
   return (
-    <div className="bg-white p-4 mr-8">
+    <div className="bg-white p-4 mr-8 border-b-2 border-blue-100">
+      <div className="flex flex-row items-center">
+        <img
+          className="inline object-cover w-10 h-10 mr-2 rounded-full"
+          src="https://cdn.vuetifyjs.com/images/john.jpg"
+          alt="Profile image"
+        />
+        <div className="flex flex-col">
+          <span className="font-medium">Katsukiniwa</span>
+          <span className="text-sm">コメント日: 2021/12/21</span>
+        </div>
+      </div>
       <p>
         子画面とはどういうものを言っていますか？
       </p>
