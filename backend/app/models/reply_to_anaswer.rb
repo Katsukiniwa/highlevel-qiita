5b593ab1bea796165dbedba5e51fc8b56755f434
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-22 23:43:26 +0900

    delete reply resource

--- backend/app/models/reply_to_anaswer.rb
+++
@@ -1,4 +0,0 @@
-class ReplyToAnaswer < ApplicationRecord
-  belongs_to :question
-  belongs_to :answer
-end
--- backend/app/models/reply_to_reply.rb
+++
@@ -1,4 +0,0 @@
-class ReplyToReply < ApplicationRecord
-  belongs_to :question
-  belongs_to :reply
-end
