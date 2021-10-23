cba9139ec0ae695255152de7887ef1be653d8be7
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-23 10:43:59 +0900

    create question hooks

---
+++ frontend/src/module/question/hooks/useQuestionFetch.ts
@@ -0,0 +1,40 @@
+import axios from "axios"
+import { useContext, useState, useEffect } from "react"
+import { QuestionContext, QuestionUpdateContext } from "../../../store/QuestionContext"
+import { actions, QuestionState } from "../reducer/questionReducer"
+import { Question } from "../type/question"
+
+export const useQuestionFetch = (): [QuestionState, () => void] => {
+  const questions = useContext(QuestionContext)
+  const dispatch = useContext(QuestionUpdateContext)
+  const [refetchIndex, setRefetchIndex] = useState(0)
+
+  const refetch = () => {
+    setRefetchIndex(prevRefetchIndex => prevRefetchIndex + 1)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!dispatch) {
+        return
+      }
+      
+      dispatch(actions.startFetchQuestionAction())
+      
+      axios.get<Question[]>("/novels")
+        .then(res => {
+          return res.data
+        })
+        .then(data => {
+          dispatch(actions.successFetchQuestionAction(data))
+        })
+        .catch((e) => {
+          dispatch(actions.failFetchQuestionAction(e))
+        })
+    }
+
+    fetchData()
+  }, [dispatch, refetchIndex])
+
+  return [questions, refetch]
+}
