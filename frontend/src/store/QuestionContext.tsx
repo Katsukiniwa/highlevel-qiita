d6f7f70a2890e1eebd2c34f9c413aa630cafbffc
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-23 10:42:54 +0900

    create question context

---
+++ frontend/src/store/QuestionContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, Dispatch, ReactChild, useReducer } from "react"
+import { QuestionActionType, QuestionReducer, QuestionState } from "../module/question/reducer/questionReducer"
+
+export const QuestionContext = createContext<QuestionState>({ isLoading: false, questions: [], error: null })
+
+export const QuestionUpdateContext = createContext<Dispatch<QuestionActionType>>(() =>{})
+
+export const initialAuthenticationState = { isLoading: false, questions: [], error: null }
+
+export function QuestionContextProvider({ children }: { children: ReactChild }) {
+  const [questions, questionDispatch] = useReducer(QuestionReducer, initialAuthenticationState)
+
+  return (
+    <QuestionContext.Provider value={questions}>
+      <QuestionUpdateContext.Provider value={questionDispatch}>
+        {children}
+      </QuestionUpdateContext.Provider>
+    </QuestionContext.Provider>
+  )
+}
