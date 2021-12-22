5e7fcd7aafbb0e373ad28e0704d19eb107846c2c
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-22 11:21:41 +0900

    update seed

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_053850) do
+ActiveRecord::Schema.define(version: 2021_12_22_010616) do
 
   create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -56,6 +56,13 @@ ActiveRecord::Schema.define(version: 2021_12_21_053850) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_favorite_questions_on_question_id"
     t.index ["user_id"], name: "index_favorite_questions_on_user_id"
+  end
+
+  create_table "links", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "url"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -80,4 +80,7 @@ Question.all.each do |q|
   end
 end
 
+Link.create(url: 'http://graphql.org/', description: 'The Best Query Language')
+Link.create(url: 'http://dev.apollodata.com/', description: 'Awesome GraphQL Client')
+
 puts "end seed"
