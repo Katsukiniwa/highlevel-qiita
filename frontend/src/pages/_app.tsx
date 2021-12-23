3f5ad5679907633cc33daea7929203a3e106c038
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-24 00:28:30 +0900

    run codegen

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 
 import type { ReactChild, ReactElement, ReactNode } from 'react'
 import { AuthenticationContextProvider } from '../store/AuthenticationContext'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { QuestionContextProvider } from '../store/QuestionContext'
@@ -19,14 +20,22 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   MockServer();
 }
 
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
+  cache,
+});
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <AuthenticationContextProvider>
-      <QuestionContextProvider>
-        {getLayout(<Component {...pageProps} />)}
-      </QuestionContextProvider>
-    </AuthenticationContextProvider>
+    <ApolloProvider client={client}>
+      <AuthenticationContextProvider>
+        <QuestionContextProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </QuestionContextProvider>
+      </AuthenticationContextProvider>
+    </ApolloProvider>
   )
 }
