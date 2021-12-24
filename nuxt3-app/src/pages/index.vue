2a28e8ff034a65d1ac77dd625f0a3f0635d77e00
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-24 13:31:28 +0900

    install eslint and prettier

---
+++ nuxt3-app/src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="text-xl font-bold mb-2">Notes</h1>
+    <p class="mb-4">built with nuxt 3</p>
+    <ul class="border shadow divide-y">
+      <li v-for="user of data" :key="user.id">
+        <NuxtLink :to="`${user.id}`">
+          <span>{{ user.title }}</span>
+          <span>completed: {{ user.completed }}</span>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData('count', () =>
+  $fetch('https://jsonplaceholder.typicode.com/todos')
+);
+</script>
+
+<style></style>
