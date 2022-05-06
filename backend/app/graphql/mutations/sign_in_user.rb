c40bfa1172327ea186d9c9b38a9e33eed9a4f9b4
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-05-07 00:03:49 +0900

    throw error signInUser when invalid password or email

--- backend/app/graphql/mutations/sign_in_user.rb
+++ backend/app/graphql/mutations/sign_in_user.rb
@@ -15,7 +15,7 @@ module Mutations
 
       # ensure we have the correct user
       return unless user
-      return unless user.authenticate(credentials[:password])
+      return raise GraphQL::ExecutionError.new('invalid email or password', extensions: { code: 'AUTHENTICATION_ERROR' }) unless user.authenticate(credentials[:password])
 
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secret_key_base.byteslice(0..31))
