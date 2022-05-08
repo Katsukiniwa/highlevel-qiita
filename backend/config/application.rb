70bcdcd85378a30d9487f4a9d9b52bfe218da42c
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-05-08 10:54:29 +0900

    update cookie setting

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -39,7 +39,7 @@ module Backend
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
-    config.action_dispatch.cookies_same_site_protection = :none
+    # config.action_dispatch.cookies_same_site_protection = :none
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
