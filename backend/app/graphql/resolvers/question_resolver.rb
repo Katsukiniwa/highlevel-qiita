60cf402d10433dbe50c717c7b060297b76c09544
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-05-10 11:38:17 +0900

    add posted by me field

--- backend/app/graphql/resolvers/question_resolver.rb
+++ backend/app/graphql/resolvers/question_resolver.rb
@@ -6,7 +6,18 @@ module Resolvers
     argument :id, ID, required: true
 
     def resolve(id:)
-      Question.find(id)
+      question = Question.find(id)
+
+      {
+        id: question.id,
+        title: question.title,
+        content: question.content,
+        user: question.user,
+        created_at: question.created_at,
+        updated_at: question.updated_at,
+        comments: question.comments,
+        postedByMe: question.user.id == context[:current_user].id
+      }
     end
   end
 end
--- backend/app/graphql/types/question_type.rb
+++ backend/app/graphql/types/question_type.rb
@@ -1,12 +1,14 @@
 module Types
   class QuestionType < Types::BaseObject
-    field :id, Int, null: false
+    field :id, ID, null: false
+    field :category, CategoryType, null: false
     field :title, String, null: false
     field :content, String, null: false
     field :user, UserType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :comments, [CommentType], null: false
+    field :postedByMe, Boolean, null: false
   end
 
   class QuestionListPageType < Types::BaseObject
